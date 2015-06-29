d07827a37aac61c4542813a5bb6037d3ccfb9242
Vivek <vivekpatna52@gmail.com>
2015-06-29 15:44:27 +0530

    added node

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -7,5 +7,5 @@ node_name                "kevivmatrix"
 client_key               "#{current_dir}/kevivmatrix.pem"
 validation_client_name   "learningchef-validator"
 validation_key           "#{current_dir}/learningchef-validator.pem"
-chef_server_url          "https://default-centos65.vagrantup.com/organizations/learningchef"
+chef_server_url          "https://default-centos65/organizations/learningchef"
 cookbook_path            ["#{current_dir}/../cookbooks"]
